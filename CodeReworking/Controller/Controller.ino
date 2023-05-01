ce2a4f3f3866d81672fb5687b56f810e6d9f22a2
Michal Miskolci <misoblack300@gmail.com>
2023-05-01 19:07:39 +0200

    Made an empty controller project

---
+++ CodeReworking/Controller/Controller.ino
@@ -0,0 +1,18 @@
+/*****************************************************************//**
+ * \file   Controller.ino
+ * \brief  Main program file
+ * \details Here is void setup() and void loop()
+ * 
+ * \author xmisko06
+ * \date   May 2023
+ *********************************************************************/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+
+}
